b106d6326062e48e8774406791622eaefafd6648
Italo Maia <italo.maia@gmail.com>
2019-03-12 23:46:10 +0100

    Build is now by distro; version can be provided now

--- build.lua
+++ build.lua
@@ -1,4 +1,4 @@
-local cmd = "docker build . -t %s/lua-%s:%s -f %s --build-arg LUA_VER=%s"
+local cmd = "docker build . -t %s/lua-%s:%s -f %s --build-arg LUA_VER=%s --build-arg DISTRO_VER=%s"
 local user = os.getenv("USER")
 
 for _, lua_ver in pairs {
@@ -10,6 +10,8 @@ for _, lua_ver in pairs {
     for dockerfile in io.popen("ls Dockerfile.*"):lines() do
         local distro = string.gsub(dockerfile, "Dockerfile.", "")
 
-        os.execute(string.format(cmd, user, distro, lua_ver, dockerfile, lua_ver))
+        if arg[1] == distro then
+            os.execute(string.format(cmd, user, distro, lua_ver, dockerfile, lua_ver, arg[2] or 'latest'))
+        end
     end
 end
