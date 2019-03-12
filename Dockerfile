284785eb3139f86ace39fb6db1ac90be95959cf1
Italo Maia <italo.maia@gmail.com>
2019-03-12 21:23:29 +0100

    Adding multi distro images

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM debian:stretch-slim
-MAINTAINER Italo Maia <italo.maia@gmail.com>
-
-ARG LUA_PKG
-
-RUN apt-get update -y && apt-get install -y\
-    ${LUA_PKG}\
-    luarocks
--- build.lua
+++ build.lua
@@ -1,9 +1,15 @@
-local cmd = "docker build . -t italomaia/lua:%s --build-arg LUA_PKG=%s"
-for name, pkg in pairs {
-    ['lua51']='lua5.1',
-    ['lua52']='lua5.2',
-    ['lua53']='lua5.3',
-    ['luajit']='luajit'
+local cmd = "docker build . -t %s/lua-%s:%s -f %s --build-arg LUA_VER=%s"
+local user = os.getenv("USER")
+
+for _, lua_ver in pairs {
+    'jit',
+    '5.1',
+    '5.2',
+    '5.3',
 } do
-    os.execute(string.format(cmd, name, pkg))
+    for dockerfile in io.popen("ls Dockerfile.*"):lines() do
+        local distro = string.gsub(dockerfile, "Dockerfile.", "")
+
+        os.execute(string.format(cmd, user, distro, lua_ver, dockerfile, lua_ver))
+    end
 end
